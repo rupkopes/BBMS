e173a6799cec3649bc7fe0e2cc9133bacaceb936
rupkopes <rupevilary1010@gmail.com>
2023-10-16 21:52:17 +0545

    Add Initial Phase

---
+++ Admin Dashboard/order.js
@@ -0,0 +1,37 @@
+const Orders = [
+    {
+        orderID: '001',
+        orgName: 'Red Cross',
+        bloodType: 'B+',
+        quantity: 'Pint',
+        status: 'Pending',
+    },
+    {
+        orderID: '023',
+        orgName: 'Saathi',
+        bloodType: 'o+',
+        quantity: 'Pint',
+        status: 'Rejected',
+    },
+    {
+        orderID: '052',
+        orgName: 'Hamro LifeBank',
+        bloodType: 'A-',
+        quantity: 'Pint',
+        status: 'Pending',
+    },
+    {
+        orderID: '920',
+        orgName: 'Pharma Life',
+        bloodType: 'AB+',
+        quantity: 'Pint',
+        status: 'Approved',
+    },
+    {
+        orderID: '101',
+        orgName: 'BLODAN',
+        bloodType: 'O-',
+        quantity: 'Pint',
+        status: 'Approved',
+    }  
+]
